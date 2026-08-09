dc5a8d682c34d8aed3c700e0095403a8f338b2e7
mustafa <mustaphabhatti13@gmail.com>
2026-08-09 18:40:32 +0500

    Request Added

--- firstnode.js
+++ firstnode.js
@@ -1,9 +1,31 @@
-const html = require('html');
+const http = require('http');
 
-function requestlistener(req,res){
-    console.log(req);
+function requestlistener(req, res) {
+    console.log(req.url, req.method, req.headers);
+
+    if (req.url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write(`<h1>Welcome to the Home Page</h1>`);
+        res.end();
+    }
+
+    else if (req.url === "/about") {
+        res.setHeader("Content-Type", "text/html");
+        res.write(`<h1>Welcome to the About Page</h1>`);
+        res.end();
+    }
+
+    else {
+        res.setHeader("Content-Type", "text/html");
+        res.write(`<h1>404 Page Not Found</h1>`);
+        res.end();
+    }
 }
 
 const server = http.createServer(requestlistener);
+
 const PORT = 3001;
-server.listen(PORT,()=> {console.log("Server Listening at Port:" + PORT)});
+
+server.listen(PORT, () => {
+    console.log("Server Listening on address http://localhost:" + PORT);
+});
