6a82510ed2c3a588ef6d2ed99992ad5a83eefbf4
mustafa <mustaphabhatti13@gmail.com>
2026-08-10 09:06:57 +0500

    Corrected the Frontend Display Spaces

--- user.js
+++ user.js
@@ -10,12 +10,12 @@ function requestlistener(req, res) {
         res.write('<body><h1>Enter Your Details:</h1>');
         res.write('<form method="POST" action="/submit-details" method="POST">');
         res.write('<input type="text" id="name" name="name" placeholder="Enter Your Name"><br><br>');
-        res.write('label for="gender">Gender:</label><br>');
+        res.write('<label for="gender">Gender:</label><br>');
         res.write('<input type="radio" id="male" name="gender" value="male">');
         res.write('<label for="male">Male</label>');
         res.write('<input type="radio" id="female" name="gender" value="female">');
         res.write('<label for="female">Female</label><br>');
-        res.write('button type="submit">Submit</button>');
+        res.write('<input type="submit" value="Submit">');
         res.write('</form>');
         res.write('</body>');
         return res.end();
