cc3f9118f8830dda3dc3d9a1236d9c09e93037d8
mustafa <mustaphabhatti13@gmail.com>
2026-08-14 21:22:05 +0500

    Chapter 09 - Adding Logical Error Check

---
+++ Chapter09_Errors/logical.js
@@ -0,0 +1,8 @@
+const logicalError = () => {
+  let a = 10;
+  let b = 20;   
+   if (num = 30) { // This is a logical error, should be '==' or '===' instead of '='
+    console.log("The number is 30");
+  }
+
+  module.exports = logicalError;
