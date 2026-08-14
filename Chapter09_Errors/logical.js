00a37d866c11a402f3f6c1106cba80a1cc7af3c7
mustafa <mustaphabhatti13@gmail.com>
2026-08-14 21:24:40 +0500

    Chapter 09 - Logical File - Syntax  Error Removed

--- Chapter09_Errors/logical.js
+++ Chapter09_Errors/logical.js
@@ -4,5 +4,6 @@ const logicalError = () => {
    if (num = 30) { // This is a logical error, should be '==' or '===' instead of '='
     console.log("The number is 30");
   }
+}
 
-  module.exports = logicalError;
+module.exports = logicalError;
