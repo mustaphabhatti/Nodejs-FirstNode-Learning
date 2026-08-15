7b80e76129d118321f292cb80910b4f16599c5da
mustafa <mustaphabhatti13@gmail.com>
2026-08-15 12:44:33 +0500

    Adding Middlewares

--- Chapter09_IntroToExpress/app.js
+++ Chapter09_IntroToExpress/app.js
@@ -1,7 +1,24 @@
+//Core Modules
 const http = require('http');
+//External Modules
+const express = require('express');
+//Local Modules
 const requestlistener = require('./user');
 
-const server = http.createServer(requestlistener);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("First Middleware", req.url, req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Second Middleware", req.url, req.method);
+    next();
+});
+
+const server = http.createServer(app);
+
 const PORT = 3001;
 
 server.listen(PORT, () => {
