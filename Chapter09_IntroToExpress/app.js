2cccd6b5a206a61f92aadd6eed7663dc7a757629
mustafa <mustaphabhatti13@gmail.com>
2026-08-15 16:03:29 +0500

    Practising Routing

--- Chapter09_IntroToExpress/app.js
+++ Chapter09_IntroToExpress/app.js
@@ -5,12 +5,12 @@ const requestlistener = require('./user');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
     console.log("First Middleware", req.url, req.method);
     next();
 });
 
-app.use((req, res, next) => {
+app.use("/submit-details",(req, res, next) => {
     console.log("Second Middleware", req.url, req.method);
     res.send("<h1>Welcome to Mustafa Site - Express Test</h1>");
     next();
