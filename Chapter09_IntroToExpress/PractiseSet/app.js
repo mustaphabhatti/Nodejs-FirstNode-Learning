ae53f22f65b2f586ba73eb8f53287d3f153e62e8
mustafa <mustaphabhatti13@gmail.com>
2026-08-17 18:20:54 +0500

    Duplicate Middlewares app.post and app.use

--- Chapter09_IntroToExpress/PractiseSet/app.js
+++ Chapter09_IntroToExpress/PractiseSet/app.js
@@ -18,8 +18,8 @@ app.use("/",(req, res, next) => {
     next();
 });
 
-app.get("/contact-us",(req, res, next) => {
-    console.log("Fourth Middleware", req.url);
+app.use("/contact-us",(req, res, next) => {
+    console.log("Fourth Middleware");
     res.send("<h1>Welcome to Mustafa Site Contact Page - Express Test</h1><br>");
     res.send('<form method="POST" action="/submit-details" method="POST">');
     res.send('<input type="text" id="name" name="name" placeholder="Enter Your Name"><br><br>');
@@ -30,10 +30,25 @@ app.get("/contact-us",(req, res, next) => {
     res.send('<label for="female">Female</label>');
     res.send('</form>');
     next();
-}
+});
+
+app.post("/contact-us",(req, res, next) => {
+    console.log("Fourth Middleware");
+    res.send("<h1>Welcome to Mustafa Site Contact Page - Express Test</h1><br>");
+    res.send('<form method="POST" action="/submit-details" method="POST">');
+    res.send('<input type="text" id="name" name="name" placeholder="Enter Your Name"><br><br>');
+    res.send('<label for="gender">Gender:</label><br>');
+    res.send('<input type="radio" id="male" name="gender" value="male">');
+    res.send('<label for="male">Male</label>');
+    res.send('<input type="radio" id="female" name="gender" value="female">');
+    res.send('<label for="female">Female</label>');
+    res.send('</form>');
+    next();
+});
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
+   
     console.log("Server Listening on address http://localhost:" + PORT);
 });
