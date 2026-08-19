c620e8ad469bf53b812d16751d4e44a7093ee916
mustafa <mustaphabhatti13@gmail.com>
2026-08-19 11:25:29 +0500

    Airbnb Home Page - Setup Middleware

--- Chapter10_airbnb/app.js
+++ Chapter10_airbnb/app.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.use((req, res, next) => {
     console.log(req.url, req.methods);
+    next();
+});
+
+app.get("/", (req, res, next) => {
     res.send(
         `<h1>Welcome to Airbnb Clone Project</h1>
         <a href="/add-home">Add Home</a>`
+    );
+    next();
+});
+
+app.get("/add-home", (req, res, next) => {
+    res.send(
+        `<h1>Add Home</h1>
+        <a href="/">Back to Home</a>`
     );
     next();
 });
