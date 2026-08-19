7fc59a7c2c90913c8ee4d799925198c66cb39260
mustafa <mustaphabhatti13@gmail.com>
2026-08-19 11:57:11 +0500

    Added Host Router

---
+++ Chapter10_airbnb/routes/hostRouter.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hostRouter = express.Router();
+
+
+hostRouter.get("/host/add-home", (req, res, next) => {
+    res.send(
+        `<h1>Register Your Home</h1>
+        <form method="POST" action="/host/add-home">
+            <input type="text" name="title" placeholder="Home Title" required>
+            <input type="text" name="description" placeholder="Home Description" required>
+            <input type="text" name="location" placeholder="Location" required>
+            <input type="number" name="price" placeholder="Price per night" required>
+            <button type="submit">Register Home</button>
+        </form>`
+    );
+    next();
+});
+
+hostRouter.post("/host/add-home", (req, res, next) => {
+    console.log("Home Details Received:", req.body);
+    res.send(
+        `<h1>Home Registered Successfully</h1>
+        <a href="/host/add-home">Add Another Home</a>
+        <a href="/">Go Back to Home</a>`
+    );
+    next();
+});
+
+
+module.exports = hostRouter;
