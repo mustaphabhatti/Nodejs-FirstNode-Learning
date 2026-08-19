b518fef4d56293592973eec1fee291d348751daa
mustafa <mustaphabhatti13@gmail.com>
2026-08-19 12:39:30 +0500

    Added Status 404 Pages

--- Chapter10_airbnb/app.js
+++ Chapter10_airbnb/app.js
@@ -6,16 +6,13 @@ const hostRouter = require('./routes/hostRouter');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.url, req.methods);
-    next();
-});
-
 app.use(express.urlencoded());
-
 app.use(userRouter);
-
 app.use(hostRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Error - Page Not Found</h1>");
+});
 
 const PORT = 3000;
 
