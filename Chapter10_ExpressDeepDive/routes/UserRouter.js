a16ca1b064c816c1b0326ec1b994eff5bff5c41b
mustafa <mustaphabhatti13@gmail.com>
2026-08-20 19:37:24 +0500

    HTML Files Added

--- Chapter10_ExpressDeepDive/routes/UserRouter.js
+++ Chapter10_ExpressDeepDive/routes/UserRouter.js
@@ -7,13 +7,11 @@ const userRouter = express.Router();
 userRouter.use(bodyParser.urlencoded({ extended: false }));
 
 userRouter.use("/", (req, res, next) => {
-
+    res.sendFile(path.join(rootDir, "views", "index.html"));
 });
 
 userRouter.use("/submit-details",(req, res, next) => {
-    console.log("Second Middleware", req.url, req.method);
-    res.send("<h1>Welcome to Mustafa Site - Express Test</h1>");
-    next();
+    res.sendFile(path.join(rootDir, "views", "submitdetails.html"));
 });
 
 module.exports = userRouter;
