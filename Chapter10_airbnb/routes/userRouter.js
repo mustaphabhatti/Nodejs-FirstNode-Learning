397f71a7a04c470823c0f9b29452c396b663f5de
mustafa <mustaphabhatti13@gmail.com>
2026-08-20 12:44:45 +0500

    Updating Paths in UserRouter

--- Chapter10_airbnb/routes/userRouter.js
+++ Chapter10_airbnb/routes/userRouter.js
@@ -3,9 +3,11 @@ const path = require("path");
 //Main Module for the Airbnb Application
 const express = require('express');
 const userRouter = express.Router();
+//Local Modules
+const rootDir = require('../utils/pathUtil');
 
 userRouter.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../views/home.html"));
+    res.sendFile(path.join(rootDir, "views", "home.html"));
 });
 
 module.exports = userRouter;
