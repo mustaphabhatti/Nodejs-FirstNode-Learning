9d2b4978586077802de6a757f2942527158a40da
mustafa <mustaphabhatti13@gmail.com>
2026-08-20 12:39:26 +0500

    Adding Root Directory

--- Chapter10_airbnb/app.js
+++ Chapter10_airbnb/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 //Local Modules
+const rootDir = require('./utils/pathUtil');
 const userRouter = require('./routes/userRouter');
 const hostRouter = require('./routes/hostRouter');
 
@@ -12,7 +13,7 @@ app.use(userRouter);
 app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 const PORT = 3000;
