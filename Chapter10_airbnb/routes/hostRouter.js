c026b07bce1ab8381e8ae6297709c4c1fb85d90e
mustafa <mustaphabhatti13@gmail.com>
2026-08-20 12:44:08 +0500

    Updating Paths in HostRouter

--- Chapter10_airbnb/routes/hostRouter.js
+++ Chapter10_airbnb/routes/hostRouter.js
@@ -2,14 +2,15 @@
 const path = require('path');
 const express = require('express');
 const hostRouter = express.Router();
-
+//Local Modules
+const rootDir = require('../utils/pathUtil');
 
 hostRouter.get("/add-home", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../views/addhome.html"));
+    res.sendFile(path.join(rootDir, "views", "addhome.html"));
 });
 
 hostRouter.post("/add-home", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../views/homeAdded.html"));
+    res.sendFile(path.join(rootDir, "views", "homeAdded.html"));
 });
 
 
